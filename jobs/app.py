3db81c253353ff6765955acd21315d3e19e2f341
saddique164 <saddique_uet@yahoo.com>
2022-12-01 00:12:32 +0100

    database connection added added

--- jobs/app.py
+++ jobs/app.py
@@ -12,6 +12,11 @@ def open_connection():
     connection.row_factory=sqlite3.Row
     return connection
 
+def execute_sql():
+    connection = open_connection()
+    
+    return connection
+
 
 # @app.teardown_appcontext
 # def close_connection(exception):
