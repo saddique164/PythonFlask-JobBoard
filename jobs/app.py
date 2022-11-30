25cbfe07ed1c0b5b96d5d01c34230265a212c94d
saddique164 <saddique_uet@yahoo.com>
2022-12-01 00:08:29 +0100

    database connection added added

--- jobs/app.py
+++ jobs/app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, g
 import sqlite3
 
-PATH="db/jobs.sqlite"
+PATH='db/jobs.sqlite'
 
 app = Flask(__name__)
 
 def open_connection():
-    if connection == getattr(g._connection,"None"):
-       connection = getattr(g._connection,sqlite3.connect(PATH))
+    connection == getattr(g, '_connection', None):
+    if connection is None:
+       connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory=sqlite3.Row
     return connection
+
+
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
 
 @app.route('/')
 @app.route('/jobs')
