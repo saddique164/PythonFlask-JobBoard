4fdcd4934cff690d808f3b1ddfcdbecacfe45fca
saddique164 <saddique_uet@yahoo.com>
2022-11-30 23:56:10 +0100

    layout.html added

--- jobs/app.py
+++ jobs/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, g
 import sqlite3
 
+PATH="db/jobs.sqlite"
+
 app = Flask(__name__)
 
+def open_connection():
+    if connection == getattr(g._connection,"None"):
+       connection = getattr(g._connection,sqlite3.connect(PATH))
+    return connection
+
 @app.route('/')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
-
-@app.route()
-def open_connection():
-    connection = getattr(g._connection,"None")
