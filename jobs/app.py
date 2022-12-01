8a53abe4f1835f55ffc324458fe1fc20713ec6d6
saddique164 <saddique_uet@yahoo.com>
2022-12-01 20:14:50 +0100

    database connection query added

--- jobs/app.py
+++ jobs/app.py
@@ -7,22 +7,27 @@ app = Flask(__name__)
 
 def open_connection():
     connection == getattr(g, '_connection', None)
-    if connection is None:
+    if connection == None:
        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory=sqlite3.Row
     return connection
 
-def execute_sql():
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    sql=0
-    values = ()
-    commit = False
-    single = False
     cursor = connection.execute(sql,values)
     if commit is True:
         results = connection.commit()
     else:
-        results = 'select * from table where if: cursor.fetchone() if single else cursor.fetchall()' 
+        results=cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
 
 
 
