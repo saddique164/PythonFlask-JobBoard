27d268069f9aff4880d1e84538907aebdc5fcd5e
saddique164 <saddique_uet@yahoo.com>
2022-12-02 22:12:28 +0100

    macro job  added

--- jobs/app.py
+++ jobs/app.py
@@ -41,4 +41,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description,job.salary,employer.id as employer_id, employer.name as employer_name From job Join employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
